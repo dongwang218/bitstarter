cc2ad195435263a15cd3a31b5561ef24d1004c91
Dong Wang <dong@databricks.com>
2015-10-18 00:35:43 -0700

    manually add testgithub2.scala

---
+++ testgithub2.scala
@@ -0,0 +1,18 @@
+// Databricks notebook source exported at Sun, 18 Oct 2015 07:26:30 UTC
+// dbadmin can edit this
+
+// COMMAND ----------
+
+1 + 1
+
+// COMMAND ----------
+
+// MAGIC %sql show tables;
+
+// COMMAND ----------
+
+// MAGIC %sql select * from sales_long
+
+// COMMAND ----------
+1 +2 +3
+
