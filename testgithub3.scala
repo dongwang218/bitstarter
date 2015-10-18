6ab758902ad299f6bc052375ddb389ab5363dfee
dongwang218 <dong@databricks.com>
2015-10-18 10:36:47 -0700

    initial commit

---
+++ testgithub3.scala
@@ -0,0 +1,18 @@
+// Databricks notebook source exported at Sun, 18 Oct 2015 17:36:47 UTC
+// dbadmin can edit this
+
+// COMMAND ----------
+
+1 + 1
+
+// COMMAND ----------
+
+// MAGIC %sql show tables;
+
+// COMMAND ----------
+
+// MAGIC %sql select * from sales_long
+
+// COMMAND ----------
+
+1 +2 +3
